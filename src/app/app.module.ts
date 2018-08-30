37d0cd1f81c84a84a40e76d9130d91c57e2c8e5d
Darius Scheepers <dscheepers@irodo.com>
2018-08-30 17:14:46 +0200

    oh oh

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-<<<<<<< HEAD
 import { Http } from '../http-api';
 import { HttpModule} from '@angular/http';
-=======
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
-<<<<<<< HEAD
 import { LoginPage } from '../pages/login/login';
 import { RegisterPage } from '../pages/register/register';
 import { AnnouncementsPage } from '../pages/announcements/announcements';
-=======
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -24,45 +18,30 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     HomePage,
-<<<<<<< HEAD
     ListPage,
     LoginPage,
     AnnouncementsPage,
     RegisterPage
-=======
-    ListPage
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-<<<<<<< HEAD
     HttpModule
-=======
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-<<<<<<< HEAD
     ListPage,
     LoginPage,
     AnnouncementsPage,
     RegisterPage
-=======
-    ListPage
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
   ],
   providers: [
     StatusBar,
     SplashScreen,
-<<<<<<< HEAD
     {provide: ErrorHandler, useClass: IonicErrorHandler},
     Http
-=======
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
->>>>>>> 1d4376261b0c70109c709e4ecac9dfd29e38e90f
   ]
 })
 export class AppModule {}
